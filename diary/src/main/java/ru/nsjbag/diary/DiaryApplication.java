367f175cb62e9dd47e5ceb4dc17ba039c60fb414
Dmitry Subbotin <sybbotin1234@yandex.ru>
2024-04-06 00:39:15 +0300

    init diary web app

---
+++ diary/src/main/java/ru/nsjbag/diary/DiaryApplication.java
@@ -0,0 +1,13 @@
+package ru.nsjbag.diary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiaryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiaryApplication.class, args);
+	}
+
+}
---
+++ diary/src/test/java/ru/nsjbag/diary/DiaryApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.nsjbag.diary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiaryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
