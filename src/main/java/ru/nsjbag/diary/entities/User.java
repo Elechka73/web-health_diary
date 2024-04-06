1db17c528854b150c7364a916af01fe84cd2cbeb
Dmitry Subbotin <sybbotin1234@yandex.ru>
2024-04-06 11:45:56 +0300

    add new entities

---
+++ src/main/java/ru/nsjbag/diary/entities/User.java
@@ -0,0 +1,21 @@
+package ru.nsjbag.diary.entities;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    private String username;
+
+    private String password;
+
+    private String firstname;
+
+    @OneToOne(mappedBy = "user", cascade = CascadeType.ALL)
+    private Authority authority;
+}
