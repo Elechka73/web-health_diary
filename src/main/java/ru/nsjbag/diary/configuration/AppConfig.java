e64cb2e56071a4ba0fb923edd127a839240c71de
Dmitry Subbotin <sybbotin1234@yandex.ru>
2024-04-06 11:37:39 +0300

    add AppConfig

---
+++ src/main/java/ru/nsjbag/diary/configuration/AppConfig.java
@@ -0,0 +1,9 @@
+package ru.nsjbag.diary.configuration;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("ru.nsjbag.diary")
+public class AppConfig {
+}
