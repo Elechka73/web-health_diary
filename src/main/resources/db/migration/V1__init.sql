b55e1e270654b0f3ac98e6042ed23cbc86d9ee97
Dmitry Subbotin <sybbotin1234@yandex.ru>
2024-04-29 22:03:55 +0300

    update navigation and migration file add displaying first and last name in user menu in navbar, update migration (auto create diary for default user)

--- src/main/resources/db/migration/V1__init.sql
+++ src/main/resources/db/migration/V1__init.sql
@@ -3,12 +3,16 @@ CREATE TABLE users
     username         VARCHAR(50)  NOT NULL,
     password         VARCHAR(100) NOT NULL,
     firstname        VARCHAR(30)  NOT NULL,
+    lastname         VARCHAR(30)  NOT NULL,
     enabled          boolean      NOT NULL,
-    PRIMARY KEY (username)
+    patronymic       VARCHAR(30),
+    date_of_birth    date        NOT NULL,
+    insurance_number varchar(16) NOT NULL,
+        PRIMARY KEY (username)
 );
-INSERT INTO users(username, password, firstname, enabled)
-VALUES ('elvira', '$2a$10$5Uz2qq8W.NLKITMFizNJM.QomdCNeiRFpRqhwRvIAmv7AWiZdGMxC', 'Эльвира',  true),
-       ('user', '$2a$10$pmSqleuQX0M6hxYDQcd6P.CwHaix8gSxYiTU4pcZaOtDcz4pnb8em', 'Иван',  true);
+INSERT INTO users(username, password, firstname, lastname, patronymic, date_of_birth,insurance_number, enabled )
+VALUES ('elvira', '$2a$10$5Uz2qq8W.NLKITMFizNJM.QomdCNeiRFpRqhwRvIAmv7AWiZdGMxC', 'Эльвира','Веккер', 'Викторовна','16.07.2002'::date,'1234123412341234',  true),
+       ('user', '$2a$10$pmSqleuQX0M6hxYDQcd6P.CwHaix8gSxYiTU4pcZaOtDcz4pnb8em', 'Иван','Иванов',null, '01.01.2001'::date,'0000000000000000', true);
 CREATE TABLE authorities
 (
     username  varchar(50) NOT NULL,
@@ -24,3 +28,13 @@ CREATE TABLE authorities
 INSERT INTO authorities(username, authority)
 VALUES ('elvira', 'ROLE_USER'),
        ('user', 'ROLE_MED');
+
+CREATE SEQUENCE diaries_id_seq;
+CREATE TABLE diaries
+(id INTEGER PRIMARY KEY DEFAULT nextval('diaries_id_seq') ,
+username varchar(30) NOT NULL REFERENCES users(username)
+);
+
+INSERT INTO diaries(id, username)
+VALUES
+(1, 'elvira');
